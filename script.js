7076ae78f25f1e5385a8c0fc8aa3bb8ad0ec7c60
Srejon Sharma <srejonboss2@gmail.com>
2025-03-26 16:08:48 +0600

    final changes

--- script.js
+++ script.js
@@ -8,6 +8,30 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = textBox.value;
         containerList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     textBox.value = '';
+    saveData();
 }
+
+containerList.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", containerList.innerHTML)
+};
+
+function showList(){
+    containerList.innerHTML = localStorage.getItem("data");
+}
+showList();
